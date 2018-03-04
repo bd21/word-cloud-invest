c1259d9d43ca0cfac3c4ad857e5785e5868b43f8
Blake <bd21@uw.edu>
2018-03-04 09:47:08 -0800

    update

---
+++ main.py
@@ -0,0 +1,13 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas
+import numpy
+
+print("Running...")
+
+plt.plot(1,3)
+
+
+if __name__ == '__main__':
+    print("...End")
+    # app.run(host='127.0.0.1', port=8080, debug=True)
